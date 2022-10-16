968992a37a9d237d1f4a3b5cdb8854c902309b54
Maksim Tarasov <boxowls@gmail.com>
2022-10-16 15:05:11 +0400

    terraform

---
+++ terraform-1/variables.tf
@@ -0,0 +1,15 @@
+variable project {
+description = "maxotus"
+}
+variable region {
+description = "europe-west-1"
+# Значение по умолчанию
+default = "europe-west1"
+}
+variable public_key_path {
+# Описание переменной
+description = "/Users/maximpacts/.ssh/id_rsa.pub"
+}
+variable disk_image {
+description = "reddit-base"
+}
